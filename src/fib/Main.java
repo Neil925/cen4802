79cd75008472639d65dda0e636860e9b309f0619
Neil E. <elkadineil@gmail.com>
2025-01-08 16:20:50 -0500

    Added Main java class and fib code.

---
+++ src/fib/Main.java
@@ -0,0 +1,26 @@
+package fib;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("What number would you like to fib?:  ");
+    int n = scanner.nextInt();
+
+    System.out.println("Fib of " + n + " is: " + fib(n));
+
+    scanner.close();
+  }
+
+  public static int fib(int n) {
+    if (n == 0)
+      return 0;
+
+    if (n == 1)
+      return 1;
+
+    return fib(n - 1) + fib(n - 2);
+  }
+}
